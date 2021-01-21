5910470e7243a1e1db5f0031760a265ef2940d85
Andrey Lahnan <52515558+nanhal@users.noreply.github.com>
2021-01-21 20:41:11 +0700

    Update SplashScreen

--- app/src/main/java/com/example/e_parking/MainActivity.kt
+++ app/src/main/java/com/example/e_parking/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.e_parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "welcome",Toast.LENGTH_SHORT).show()
     }
 }
