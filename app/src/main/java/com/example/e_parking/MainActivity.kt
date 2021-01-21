12f51f7f291e0ac9e8f45d2654ff0e38ebb9dafc
abdulArif <52515011+Abdul18TI@users.noreply.github.com>
2021-01-22 02:31:40 +0700

    UILoginDanRegis

--- app/src/main/java/com/example/e_parking/MainActivity.kt
+++ app/src/main/java/com/example/e_parking/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.e_parking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
+    textView.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    });
 }
