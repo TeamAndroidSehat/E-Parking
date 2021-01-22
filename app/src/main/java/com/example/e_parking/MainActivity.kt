c719e10d9a238d99199d74698ef29f0e0c0b5553
abdulArif <52515011+Abdul18TI@users.noreply.github.com>
2021-01-22 20:06:06 +0700

    update button

--- app/src/main/java/com/example/e_parking/MainActivity.kt
+++ app/src/main/java/com/example/e_parking/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import com.example.e_parking.R.id.register_button
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_register.*
 
@@ -17,8 +20,10 @@ class MainActivity : AppCompatActivity() {
         //Toast.makeText(this, "welcome",Toast.LENGTH_SHORT).show()
         //setContentView(R.layout.activity_register)
 
-        register_button.setOnClickListener{
-            setContentView(R.layout.activity_main)
+        val button2 = findViewById<ImageButton>(R.id.register_button)
+        button2.setOnClickListener{
+            val intent1 = Intent(this, LoginActivity::class.java)
+            startActivity(intent1)
         }
 
         val button1 = findViewById<TextView>(R.id.signup)
