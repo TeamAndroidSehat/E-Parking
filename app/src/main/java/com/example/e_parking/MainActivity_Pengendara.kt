ed4ca607df35bf2f2c1e3c394e3b74d6fc9a5960
nanhal <andrey18ti@mahasiswa.pcr.ac.id>
2021-01-23 21:30:59 +0700

    Commit

---
+++ app/src/main/java/com/example/e_parking/MainActivity_Pengendara.kt
@@ -0,0 +1,35 @@
+package com.example.e_parking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity_Pengendara : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+        
+        val BerandaPengendaraFragment = Beranda_Pengendara()
+
+        makeCurrentFragment(BerandaPengendaraFragment)
+        bottomNavigationView.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.miHome -> makeCurrentFragment(BerandaPengendaraFragment)
+            }
+            true
+        }
+
+    }
+    private fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
+    }
+
+
+
+}
