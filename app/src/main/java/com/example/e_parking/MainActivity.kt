0192c7830505dc44f5ecbe83df136d90c5800850
nanhal <andrey18ti@mahasiswa.pcr.ac.id>
2021-01-23 21:23:03 +0700

    Update

--- app/src/main/java/com/example/e_parking/MainActivity.kt
+++ app/src/main/java/com/example/e_parking/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.e_parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem(2).isEnabled = false
+        
+        val BerandaPengendaraFragment = Beranda_Pengendara()
+
+        makeCurrentFragment(BerandaPengendaraFragment)
+        bottomNavigationView.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.miHome -> makeCurrentFragment(BerandaPengendaraFragment)
+            }
+            true
+        }
+
     }
+    private fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
+    }
+
 
 
 }
