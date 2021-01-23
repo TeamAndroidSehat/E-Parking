540af505be64bf0f7fbc59f61c63e54a6714a388
nanhal <andrey18ti@mahasiswa.pcr.ac.id>
2021-01-23 21:36:22 +0700

    Summary

--- app/src/main/java/com/example/e_parking/MainActivity_Pengendara.kt
+++ app/src/main/java/com/example/e_parking/MainActivity_Pengendara.kt
@@ -3,12 +3,12 @@ package com.example.e_parking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_pengendara_beranda.*
 
 class MainActivity_Pengendara : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pengendara_beranda)
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem(2).isEnabled = false
         
